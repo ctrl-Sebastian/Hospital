9a78b05b39ad8734e9a4bd0e35f23dee769428a9
Sebastian Marrera <smarrera@cincinnatus.edu.do>
2024-01-11 11:46:23 -0400

    inicio usuario y caja

--- hospital-client/src/pages/BillsPage.jsx
+++ hospital-client/src/pages/BillsPage.jsx
@@ -1,6 +1,6 @@
 function BillsPage() {
   return (
-    <div>
+    <div className='flex flex-wrap items-center justify-center my-5'>
         <h1 className="text-4xl font-bold">Bills page</h1>
     </div>
   )
