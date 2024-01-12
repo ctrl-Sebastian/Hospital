e376d1e8d3b93f19ca701f0ae961463afbf36ef1
Sebastian Marrera <smarrera@cincinnatus.edu.do>
2024-01-12 00:28:59 -0400

    transacciones son visibles

--- hospital-client/src/pages/AdminPanel.jsx
+++ hospital-client/src/pages/AdminPanel.jsx
@@ -5,6 +5,7 @@ import { getPatients, deletePatient, getTransactionsRequest } from '../api/admin
 function AdminPanel() {
     const [patients, setPatients] = useState([])
     const [transactions, setTransactions] = useState([])
+    const [view, setView] = useState("");
     const navigate = useNavigate()
     
     useEffect(() => {
@@ -23,7 +24,7 @@ function AdminPanel() {
             try {
                 const res = await getTransactionsRequest()
                 if (res.status === 200) {
-                    //setTransactions(res.data);
+                    setTransactions(res.data);
                     console.log(res.data);
                 }
             } catch (error) {
@@ -45,26 +46,50 @@ function AdminPanel() {
   return (
     <div className='flex flex-wrap items-center justify-center my-5'>
         <div className='max-w-md w-full text-center'>
+        <button onClick={() => setView("users")} className="bg-zinc-500 font-bold px-4 mx-3 py-1 rounded-sm">Users</button>
 
-        <h1 className='text-4xl'>All Users</h1>
-        <Link to="/register" className="bg-green-500 font-bold px-4 py-1 rounded-sm">Register patient</Link>
+        <button onClick={() => setView("transactions")} className="bg-zinc-500 font-bold mx-3 px-4 py-1 rounded-sm">Transactions</button>
+
         {
-            patients.map((patient) => {
-                return(
-                    <div key={patient._id} className='bg-zinc-700 max-w-md w-full p-10 rounded-md my-2'>
-                        <Link to={`/users/${patient._id}`} >
-                            <div className="bg-zinc-500 px-4 py-1 rounded-md">
-                                <h1><strong>Cedula:</strong> {patient.cedula}</h1>
-                                <h1><strong>Username:</strong> {patient.username}</h1>
-                                <h2><strong>Email:</strong> {patient.email}</h2>
+            (view === "users") ? (
+                <section>
+                <h1 className='text-4xl my-3 '>Todos los usuarios: </h1>
+                <Link to="/register" className="bg-green-800 font-bold px-4 py-1 rounded-sm">Registrar paciente</Link>
+                {
+                    patients.map((patient) => {
+                        return(
+                            <div key={patient._id} className='bg-zinc-700 max-w-md w-full p-10 rounded-md my-2'>
+                                <Link to={`/users/${patient._id}`} >
+                                    <div className="bg-zinc-500 px-4 py-1 rounded-md">
+                                        <h1><strong>Cedula:</strong> {patient.cedula}</h1>
+                                        <h1><strong>Username:</strong> {patient.username}</h1>
+                                        <h2><strong>Email:</strong> {patient.email}</h2>
+                                    </div>
+                                </Link>
+                                <button onClick={() => navigate(`/users/${patient._id}`)} className='bg-blue-700 p-5 rounded-md mx-2 my-1'>Update</button>
+                                <button onClick={() => deleteUser(patient._id)} className='bg-red-700 p-5 rounded-md mx-2 my-1'>Delete</button>
                             </div>
-                        </Link>
-                        <button onClick={() => navigate(`/users/${patient._id}`)} className='bg-blue-700 p-5 rounded-md mx-2 my-1'>Update</button>
-                        <button onClick={() => deleteUser(patient._id)} className='bg-red-700 p-5 rounded-md mx-2 my-1'>Delete</button>
-                    </div>
-                )
-            })
+                        )
+                    })
+                }
+                </section>
+            ) : (
+                <section>
+                <h1 className='text-4xl'>Todas las transacciones: </h1>
+                {
+                    transactions.map((transaction) => {
+                        return(
+                            <div key={transaction._id} className='bg-zinc-700 max-w-md w-full p-10 rounded-md my-2'>
+                                <h1>Paciente: {transaction.user}</h1>
+                                <h1>Cuenta pagada: {transaction.bill}</h1>
+                            </div>
+                        )
+                    })
+                }
+            </section>
+            )
         }
+
         </div>
     </div>
   )
